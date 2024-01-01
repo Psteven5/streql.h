72bcb5bd54b4b54d44381fb6a5a89af49e79831a
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-01-01 20:42:30 +0100

    Fixed bug where equality with a longer string would be true.

--- streql.h
+++ streql.h
@@ -24,14 +24,18 @@ SOFTWARE.
 
 #ifndef STREQL
 #define STREQL
+#include <stdbool.h>
 
 /*
 Checks for equality between two null-terminated strings 'a' and 'b'.
 Returns 1 if equal, else 0.
 */
 bool streql(const char* a, const char* b) {
-    while (*a == *b++ && *a) ++a;
-    return !*a;
+    while (*a == *b && *a) {
+        ++a;
+        ++b;
+    }
+    return !*a && !*b;
 }
 
 #endif
