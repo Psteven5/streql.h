a14aa525447d7a672c65b210f11c0e41d02a2b97
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-01-02 17:40:38 +0100

    Update streql.h

--- streql.h
+++ streql.h
@@ -6,35 +6,34 @@
 
 /*
 Checks for equality between two null-terminated strings 'a' and 'b'.
-Meant for long strings.
 Returns 1 if equal, else 0.
 */
 static inline bool streql(const char* a, const char* b) {
-    const unsigned long long* aq = (const unsigned long long*)a;
-    const unsigned long long* bq = (const unsigned long long*)b;
+    const uint64_t* aq = (const uint64_t*)a;
+    const uint64_t* bq = (const uint64_t*)b;
     while (*aq && *aq == *bq) {
         ++aq;
         ++bq;
     }
-    const unsigned long* ad = (const unsigned long*)aq;
-    const unsigned long* bd = (const unsigned long*)bq;
+    const uint32_t* ad = (const uint32_t*)--aq;
+    const uint32_t* bd = (const uint32_t*)--bq;
     while (*ad && *ad == *bd) {
         ++ad;
         ++bd;
     }
-    const unsigned short* aw = (const unsigned short*)ad;
-    const unsigned short* bw = (const unsigned short*)bd;
+    const uint16_t* aw = (const uint16_t*)--ad;
+    const uint16_t* bw = (const uint16_t*)--bd;
     while (*aw && *aw == *bw) {
         ++aw;
         ++bw;
     }
-    a = (const char*)aw;
-    b = (const char*)bw;
+    a = (const char*)--aw;
+    b = (const char*)--bw;
     while (*a && *a == *b) {
         ++a;
         ++b;
     }
-    return !*b;
+    return !*a && !*b;
 }
 
 #endif
