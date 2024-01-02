4f3b3aceaee8ab801c3732cacbdd30d1977b7614
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-01-02 11:29:13 +0100

    Update streql.h

--- streql.h
+++ streql.h
@@ -14,8 +14,8 @@ static inline bool streql(const char* a, const char* b) {
     do {
         al = *_a++;
         bl = *_b++;
-    } while (al == bl && al & al >> 24 & al >> 8);
-    return al == bl;
+    } while (al - bl);
+    return al - (bl << 1);
 }
 
 /*
@@ -23,23 +23,22 @@ static inline bool streql(const char* a, const char* b) {
 Checks for equality between two null-terminated strings 'a' and 'b'.
 Returns 1 if equal, else 0.
 */
-static inline bool streql_x64(const char* a, const char* b) {
+static inline bool streql(const char* a, const char* b) {
     bool ret;
     asm(
         "leaq (%1), %%rcx;"
         "leaq (%2), %%rdx;"
         ".STREQL_LOOP:"
-        "mov (%%rcx), %%ax;"
-        "mov (%%rdx), %%bx;"
-        "addq $2, %%rcx;"
-        "addq $2, %%rdx;"
-        "testb %%al, %%al;"
-        "andb %%ah, %%ah;"
+        "movl (%%rcx), %%eax;"
+        "movl (%%rdx), %%ebx;"
+        "addq $4, %%rcx;"
+        "addq $4, %%rdx;"
+        "testl %%eax, %%eax;"
         "jz .STREQL_END;"
-        "xor %%ax, %%bx;"
+        "xorl %%eax, %%ebx;"
         "jz .STREQL_LOOP;"
         ".STREQL_END:"
-        "xor %%ax, %%bx;"
+        "xorl %%eax, %%ebx;"
         "setz %0;"
         : "=r" (ret)
         : "r" (a), "r" (b)
