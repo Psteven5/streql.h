f2afe5f1196330090be97ebd267bb509f51f6b61
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-01-02 02:28:16 +0100

    Changed the function to inline assembly

--- streql.h
+++ streql.h
@@ -8,11 +8,28 @@ Checks for equality between two null-terminated strings 'a' and 'b'.
 Returns 1 if equal, else 0.
 */
 static inline bool streql(const char* a, const char* b) {
-    while (*a == *b && *a) {
-        ++a;
-        ++b;
-    }
-    return *a == *b;
+    bool result;
+    asm(
+        ".LOOP:"
+        "movb (%1), %%al;"
+        "movb (%2), %%bl;"
+        "movb +1(%1), %%ah;"
+        "movb +1(%2), %%bh;"
+        "incq %1;"
+        "incq %2;"
+        "testb %%al, %%al;"
+        "andb %%ah, %%ah;"
+        "jz .END;"
+        "xor %%ax, %%bx;"
+        "jz .LOOP;"
+        ".END:"
+        "xor %%ax, %%bx;"
+        "setz %0;"
+        : "=r" (ret)
+        : "r" (a), "r" (b)
+        : "%ax", "%bx"
+    );
+    return result;
 }
 
 #endif
