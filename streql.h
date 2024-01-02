e1672c5feedc37618c545c509c642050d21e101c
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-01-02 13:00:21 +0100

    Update streql.h

--- streql.h
+++ streql.h
@@ -2,19 +2,20 @@
 #define STREQL
 
 #include <stdbool.h>
+#include <stdint.h>
 
 /*
 Checks for equality between two null-terminated strings 'a' and 'b'.
 Returns 1 if equal, else 0.
 */
 static inline bool streql(const char* a, const char* b) {
-    const unsigned int* _a = (const unsigned int*)a;
-    const unsigned int* _b = (const unsigned int*)b;
-    unsigned int al, bl;
+    const uint32_t* _a = (const uint32_t*)a;
+    const uint32_t* _b = (const uint32_t*)b;
+    uint32_t al, bl;
     do {
         al = *_a++;
         bl = *_b++;
-    } while (al == bl && al);
+    } while (al && al == bl);
     return !bl;
 }
 
@@ -23,23 +24,21 @@ static inline bool streql(const char* a, const char* b) {
 Checks for equality between two null-terminated strings 'a' and 'b'.
 Returns 1 if equal, else 0.
 */
-static inline bool streql(const char* a, const char* b) {
+static inline bool streql_x64(const char* a, const char* b) {
     bool ret;
     asm(
-        "leaq (%1), %%rcx;"
-        "leaq (%2), %%rdx;"
         ".STREQL_LOOP:"
-        "movl (%%rcx), %%eax;"
-        "movl (%%rdx), %%ebx;"
-        "addq $4, %%rcx;"
-        "addq $4, %%rdx;"
+        "movl (%1), %%eax;"
+        "movl (%2), %%ebx;"
+        "addq $4, %1;"
+        "addq $4, %2;"
         "testl %%eax, %%eax;"
         "jz .STREQL_END;"
-        "xorl %%eax, %%ebx;"
-        "jz .STREQL_LOOP;"
+        "cmpl %%eax, %%ebx;"
+        "je .STREQL_LOOP;"
         ".STREQL_END:"
-        "xorl %%eax, %%ebx;"
-        "setz %0;"
+        "testl %%ebx, %%ebx;"
+        "setzb %0;"
         : "=r" (ret)
         : "r" (a), "r" (b)
         : "rax", "%rbx", "%rcx", "%rdx"
