661d3d165c1fdf75e20761b935d795cf8cc74930
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-01-06 17:52:52 +0100

    Update streql.h

--- streql.h
+++ streql.h
@@ -10,18 +10,16 @@ Checks for equality between two null-terminated strings 'a' and 'b'.
 Returns 1 if equal, else 0.
 */
 static inline bool streql(const char* a, const char* b) {
-    const size_t* pa = (const size_t*)a;
-    const size_t* pb = (const size_t*)b;
     size_t ai, bi;
     do {
-        memcpy(&ai, pa, sizeof(size_t));
+        memcpy(&ai, a, sizeof(size_t));
         if (!ai) break;
-        memcpy(&bi, pb, sizeof(size_t));
+        memcpy(&bi, b, sizeof(size_t));
         if (ai ^ bi) break;
-        ++pa;
-        ++pb;
+        a += sizeof(size_t);
+        b += sizeof(size_t);
     } while (1);
-    return !strncmp((const char*)pa, (const char*)pb, sizeof(size_t));
+    return !strncmp(a, b, sizeof(size_t));
 }
 
 #endif
