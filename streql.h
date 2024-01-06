6b7fbaa4df088142e207dc6048d5a3b349a73c26
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-01-06 15:57:06 +0100

    Update streql.h

--- streql.h
+++ streql.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 
 /*
 -- Only worth using for longer strings. --
@@ -10,49 +11,10 @@ Checks for equality between two null-terminated strings 'a' and 'b'.
 Returns 1 if equal, else 0.
 */
 static inline bool streql(const char* a, const char* b) {
-    const uint64_t* aq = (const uint64_t*)a;
-    const uint64_t* bq = (const uint64_t*)b;
-    const void* as = aq;
-    const void* bs = bq;
-    while (*aq && *aq == *bq) {
-        ++aq;
-        ++bq;
-    }
-    if (aq > as) {
-        --aq;
-        --bq;
-    }
-    const uint32_t* ad = (const uint32_t*)aq;
-    const uint32_t* bd = (const uint32_t*)bq;
-    as = ad;
-    bs = bd;
-    while (*ad && *ad == *bd) {
-        ++ad;
-        ++bd;
-    }
-    if (ad > as) {
-        --ad;
-        --bd;
-    }
-    const uint16_t* aw = (const uint16_t*)ad;
-    const uint16_t* bw = (const uint16_t*)bd;
-    as = aw;
-    bs = bw;
-    while (*aw && *aw == *bw) {
-        ++aw;
-        ++bw;
-    }
-    if (aw > as) {
-        --aw;
-        --bw;
-    }
-    a = (const char*)aw;
-    b = (const char*)bw;
-    while (*a && *a == *b) {
-        ++a;
-        ++b;
-    }
-    return !*a && !*b;
+    size_t* pa = (size_t*)a;
+    size_t* pb = (size_t*)b;
+    while (*pa == *pb) ++pa; ++pb;
+    return !strncmp((const char*)pa, (const char*)pb, sizeof(size_t));
 }
 
 #endif
