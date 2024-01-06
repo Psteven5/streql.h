508847b13084e7e40588c3aaf937e6449e1fd444
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-01-06 16:01:00 +0100

    Update streql.h

--- streql.h
+++ streql.h
@@ -6,14 +6,13 @@
 #include <string.h>
 
 /*
--- Only worth using for longer strings. --
 Checks for equality between two null-terminated strings 'a' and 'b'.
 Returns 1 if equal, else 0.
 */
 static inline bool streql(const char* a, const char* b) {
     size_t* pa = (size_t*)a;
     size_t* pb = (size_t*)b;
-    while (*pa == *pb) ++pa; ++pb;
+    while (*pa && !(*pa ^ *pb)) ++pa; ++pb;
     return !strncmp((const char*)pa, (const char*)pb, sizeof(size_t));
 }
 
